b5a8a6ded4d046bb49f9e549da3967c89e32b087
ToJoos <joosx2000@gmail.com>
2022-06-04 09:35:53 +0200

    Initial commit

---
+++ src/main/java/dev/tojoos/movieLibrary/MovieLibraryApplication.java
@@ -0,0 +1,13 @@
+package dev.tojoos.movieLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tojoos/movieLibrary/MovieLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tojoos.movieLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
